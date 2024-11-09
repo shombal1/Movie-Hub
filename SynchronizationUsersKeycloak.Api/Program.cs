adf42454dda97d451e11f944c935f8a93e58b251
Sasha <sasha.ruchits.06@gmail.com>
2024-11-09 18:15:10 +0300

    adding authorization and authentication based on Keycloak

--- SynchronizationUsersKeycloak.Api/Program.cs
+++ SynchronizationUsersKeycloak.Api/Program.cs
@@ -1,6 +1,44 @@
+
+using Keycloak.AuthServices.Authentication;
+using Keycloak.AuthServices.Authorization;
+using Keycloak.AuthServices.Common;
+using SynchronizationUsersKeycloak.Api;
+
 var builder = WebApplication.CreateBuilder(args);
+
+ConfigurationManager configuration = builder.Configuration;
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
+
+builder.Services.AddKeycloakWebApiAuthentication(
+    configuration,
+    options =>
+    {
+        options.RequireHttpsMetadata = false;
+        options.Audience = "account";
+    });
+
+builder.Services.AddAuthorization();
+builder.Services.AddKeycloakAuthorization(options =>
+{
+    options.EnableRolesMapping = RolesClaimTransformationSource.Realm;
+    options.RoleClaimType = KeycloakConstants.RoleClaimType;
+});
+
+builder.Services.AddSwaggerGenWithAuth(configuration);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseSwagger();
+app.UseSwaggerUI(options=>options.OAuthClientId(builder.Configuration["Keycloak:resource"]!));
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
