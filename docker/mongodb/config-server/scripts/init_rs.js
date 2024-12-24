15e2c5685889d25075e7d57099a0b40d757a8d2a
Sasha <sasha.ruchits.06@gmail.com>
2024-12-24 13:59:11 +0300

    feat: migrate from PostgreSQL to MongoDB in Docker setup

---
+++ docker/mongodb/config-server/scripts/init_rs.js
@@ -0,0 +1,12 @@
+﻿rs.initiate(
+    {
+        _id: "config_rs",
+        configsvr: true,
+        version: 1,
+        members: [
+            { _id : 0, host : "configsvr1:27017" },
+            { _id : 1, host : "configsvr2:27017" },
+            { _id : 2, host : "configsvr3:27017" }
+        ]
+    }
+)
