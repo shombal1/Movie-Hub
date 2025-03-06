e01296b35e0bf435f3d53637c2e6184beb10228a
shombal1 <sasha.ruchits.06@gmail.com>
2025-03-06 21:46:35 +0300

    refactor: Remove realms import

--- docker/run.sh
+++ docker/run.sh
@@ -9,8 +9,4 @@ docker-compose --env-file ./.env -f ./docker-compose.yml up -d
 
 sleep 30s
 
-winpty docker exec -it movie-hub-keycloak bash 
-/opt/keycloak/bin/kc.sh export import --file ./save-realm/realm-export.json
-quit
-
 sleep 1000
