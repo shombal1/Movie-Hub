3eb9506bb28273d6d82b33ffa65f59b4b0f24925
shombal1 <sasha.ruchits.06@gmail.com>
2025-06-07 21:27:46 +0300

    chore: add initial infrastructure configs
    - MongoDB:
      - Basic replica set config
      - Initial db
      - Keyfile authentication
    - Keycloak:
      - Docker setup
      - Sample realm config

---
+++ MovieHub.Host/Configs/Mongo/init-db.js
@@ -0,0 +1,27 @@
+﻿db = db.getSiblingDB('MovieHub');
+
+// Collections
+db.createCollection("Users");
+db.createCollection("Media");
+db.createCollection("MediaBasket");
+db.createCollection("Seasons");
+db.createCollection("AdditionMediaInfo");
+db.createCollection("DomainEvents");
+db.createCollection("MovieRequests");
+
+// Indexes
+db.Users.createIndex({_id: 1});
+
+db.Media.createIndex({_id: 1});
+db.Media.createIndex({releasedYearAt: 1});
+db.Media.createIndex({genres: 1});
+db.Media.createIndex({countries: 1});
+
+db.MediaBasket.createIndex({userId: 1});
+db.MediaBasket.createIndex({mediaId: 1});
+db.MediaBasket.createIndex({userId: 1, mediaId: 1}, { unique: true });
+
+db.Seasons.createIndex({seriesId: 1});
+db.AdditionMediaInfo.createIndex({mediaId: 1});
+db.DomainEvents.createIndex({_id: 1});
+db.MovieRequests.createIndex({_id: 1});
---
+++ MovieHub.Host/Configs/Mongo/init-rs.js
@@ -0,0 +1,13 @@
+﻿
+const rsConfig = {
+    _id: "rs0",
+    members: [
+        { _id: 0, host: "aaa-movie-hub-items-mongo:27017", priority: 2 },
+    ],
+};
+
+rs.initiate(rsConfig);
+
+while (!rs.isMaster().ismaster) {
+    sleep(1000);
+}
