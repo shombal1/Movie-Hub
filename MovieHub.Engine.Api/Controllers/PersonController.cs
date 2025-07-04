58dc45a2df3486978bbd85ca77db8b815c6447b2
shombal1 <sasha.ruchits.06@gmail.com>
2025-07-04 20:49:06 +0300

    feat: add PersonController

---
+++ MovieHub.Engine.Api/Controllers/PersonController.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using MovieHub.Engine.Api.Models.Responses;
+using MovieHub.Engine.Domain.UseCases.GetPerson;
+
+namespace MovieHub.Engine.Api.Controllers;
+
+[ApiController]
+[Route("api/person")]
+public class PersonController(IMediator mediator) : ControllerBase
+{
+    [HttpGet]
+    [Route("{personId}")]
+    public async Task<IActionResult> GetPerson(
+        [FromRoute] Guid personId,
+        [FromServices] IMapper mapper,
+        CancellationToken cancellationToken)
+    {
+        var query = new GetPersonQuery(personId);
+        var result = await mediator.Send(query, cancellationToken);
+
+        return Ok(mapper.Map<PersonDto>(result));
+    }
+}
---
+++ MovieHub.Engine.Api/Models/Responses/PersonDto.cs
@@ -0,0 +1,10 @@
+﻿namespace MovieHub.Engine.Api.Models.Responses;
+
+public class PersonDto : BasePersonInfoDto
+{
+    public DateOnly? BirthDate { get; set; }
+    
+    public string? Biography { get; set; }
+    
+    public IEnumerable<Guid> MediaIds { get; set; } = new List<Guid>();
+}
