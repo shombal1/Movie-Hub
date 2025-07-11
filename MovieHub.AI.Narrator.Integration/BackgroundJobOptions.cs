ceaba6babafe974c19ced5e8d523832160b98da8
shombal1 <sasha.ruchits.06@gmail.com>
2025-07-11 23:07:33 +0300

    feat: add AI Narrator microservice with Quartz scheduling

---
+++ MovieHub.AI.Narrator.Integration/BackgroundJobOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace MovieHub.AI.Narrator.Integration;
+
+public class BackgroundJobOptions
+{
+    public const string SectionName = "BackgroundJobs";
+    
+    public int MaxConcurrentJobs { get; set; } = 5;
+}
---
+++ MovieHub.AI.Narrator.Integration/Program.cs
@@ -0,0 +1,43 @@
+using MovieHub.AI.Narrator.Integration;
+using Quartz;
+
+var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+
+
+builder.Services.Configure<BackgroundJobOptions>(configuration.GetSection(BackgroundJobOptions.SectionName));
+
+builder.Services.AddQuartz(q =>
+{
+    q.UseSimpleTypeLoader();
+    q.UseDefaultThreadPool(tp => { tp.MaxConcurrency = 3; });
+    
+    q.UsePersistentStore(s =>
+    {
+        var connectionString = builder.Configuration["Quartz:ConnectionString"] 
+                               ?? throw new InvalidOperationException("Connection string for Quartz is not configured.");;
+        var tablePrefix = builder.Configuration["Quartz:TablePrefix"];
+
+        s.UseProperties = true;
+        s.RetryInterval = TimeSpan.FromSeconds(15);
+        s.UsePostgres(configurer =>
+        {
+            configurer.ConnectionString = connectionString;
+            if (!string.IsNullOrEmpty(tablePrefix))
+                configurer.TablePrefix = tablePrefix;
+        });
+        s.UseNewtonsoftJsonSerializer();
+        s.UseClustering(c =>
+        {
+            c.CheckinInterval = TimeSpan.FromSeconds(10);
+            c.CheckinMisfireThreshold = TimeSpan.FromSeconds(20);
+        });
+    });
+});
+
+builder.Services.AddQuartzHostedService(q => q.WaitForJobsToComplete = true);
+
+var app = builder.Build();
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
